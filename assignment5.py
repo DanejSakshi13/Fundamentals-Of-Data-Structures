5c30da1556c0cdda31a730f58170d9cd516416e7
DanejSakshi13 <99744219+DanejSakshi13@users.noreply.github.com>
2023-01-15 14:32:36 +0530

    Update assignment5.py

--- assignment5.py
+++ assignment5.py
@@ -1,6 +1,6 @@
-Write a Python program to store first year percentage of students in array. Write function for sorting array of floating point numbers in ascending order using
+"""Write a Python program to store first year percentage of students in array. Write function for sorting array of floating point numbers in ascending order using
 a) Selection Sort
-b) Bubble sort
+b) Bubble sort"""
 
 n = int(input("Enter the number of students:"))
 marks = []
@@ -18,28 +18,25 @@ def selectionSort(marks):
                 minIndex = j
         marks[i],marks[minIndex] = marks[minIndex] , marks[i]
     
-    for i in range(len(marks)):
-        print(marks[i])
-
+    
 def bubbleSort(marks):
     n = len(marks)
     for i in range(n-1):
         for j in range(0, n-i-1):
             if(marks[j]>marks[j+1]):
                 marks[j],marks[j+1]=marks[j+1],marks[j]
-    for i in range(len(marks)):
-        print(marks[i])
+    
 
 ch=0
 while(ch!=3):
-    ch = int(input("Enter choice : "))
+    ch = int(input("\nEnter choice : "))
     if(ch==1):
         bubbleSort(marks)
+        print("\nSorted array using bubble sort:" , marks)
     elif (ch==2):
         selectionSort(marks)
+        print("\nSorted array using selection sort:" , marks)
     elif (ch==3):
-        print("See you")
+        print("\nExit.")
     else:
         break
-
-
